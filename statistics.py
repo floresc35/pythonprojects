23a0da96a4d3b23b785e171d85c019dde211711b
Your Name <you@example.com>
2020-05-21 14:43:55 -0500

    hi

---
+++ statistics.py
@@ -0,0 +1,18 @@
+import random
+
+# This block of code randomly calculates a hero's status
+
+strength = random.randint(1,20)
+intelligence = random.randint(1,20)
+endurance = random.randint(1,20)
+wisdom = random.randint(1,20)
+
+# End random calulation code
+
+print("Your statistics are:")
+print("Strength:", strength)
+print("Intelligence:", intelligence)
+print("Endurance:", endurance)
+print("Wisdom:", wisdom)
+
+# Comment
